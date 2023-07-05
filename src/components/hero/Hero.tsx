ec4c5e57037ba2c0e25c8231e3f94af4a1a782a4
Nazrawi Medhin <nazrawimedhin1@gmail.com>
2023-07-05 09:29:31 +0300

    Hero page

---
+++ src/components/hero/Hero.tsx
@@ -0,0 +1,15 @@
+import Footer from '../Footer'
+import Head from '../Head'
+import Chiefs from '../chiefs/Chiefs'
+
+function Hero() {
+  return (
+    <div>
+      <Head/>
+      <Chiefs/>
+      <Footer/>
+    </div>
+  )
+}
+
+export default Hero
