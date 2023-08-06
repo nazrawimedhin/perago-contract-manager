d7e3ffbee087c155cadbdd8823cf30a389d88d34
Nazrawi Medhin <nazrawimedhin1@gmail.com>
2023-08-06 16:16:02 +0300

    utils

---
+++ src/utils/theme.ts
@@ -0,0 +1,11 @@
+// import { MantineTheme } from "@mantine/core";
+
+// const theme: MantineTheme = {
+//     colors: {
+//         green: ['#55ba4a', '#4da743', '#44953b', '#3b8234', '#33702c', '#2b5d25', '#224a1e', '#193816', '#11250f', '#081307'],
+
+//     },
+//     primaryColor : 'green[0]',
+// }
+
+// export default theme;
---
+++ src/utils/types.ts
@@ -0,0 +1,40 @@
+interface BaseRole {
+  id: string;
+  name: string;
+  description?: string;
+}
+
+export interface Role extends BaseRole {
+  children: Role[];
+  employees: Employee[]
+  reportsTo: Role
+}
+
+export interface CreateRole extends BaseRole {
+  parentId?: string;
+}
+
+export enum Gender {
+  Male = 'M',
+  Female = 'F',
+}
+
+export interface Employee {
+  id: string;
+  fullName: string;
+  email: string;
+  phone: string;
+  gender: Gender;
+  birthDate: Date;
+  hireDate: Date;
+  role: Role;
+  photo: string;
+}
+
+export interface EmployeeResults {
+  page: number;
+  pages: number;
+  limit: number;
+  total: number;
+  results: Employee[];
+}
