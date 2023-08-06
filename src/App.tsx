e8a8dc9458e465c83677ec85c5f217d09448e221
Nazrawi Medhin <nazrawimedhin1@gmail.com>
2023-08-06 16:14:40 +0300

    root component

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,98 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Hero from './components/hero/Hero'
-import Team from './components/team/Team'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  ColorScheme,
+  ColorSchemeProvider,
+  MantineProvider,
+} from "@mantine/core";
+import Root from "./pages/root/Root";
+import Chiefs from "./pages/chiefs/Chiefs";
+import Employees from "./pages/employees/Employees";
+import CreateRole from "./pages/forms/CreateRole";
+import CreateEmployee from "./pages/forms/CreateEmployee";
+import Employee from "./pages/employee/Employee";
+import RolesTree from "./pages/rolesTree/RolesTree";
+import EditEmployee from "./pages/forms/EditEmployee";
+import RoleGroup from "./pages/roleGroup/RoleGroup";
+import EditRole from "./pages/forms/EditRole";
+import RoleDescendants from "./pages/roleDescendants/RoleDescendants";
+import { useHotkeys, useLocalStorage } from "@mantine/hooks";
+// import theme from './utils/theme'
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Root />,
+      children: [
+        {
+          index: true,
+          element: <RolesTree />,
+        },
+        {
+          path: "/roleGroup",
+          element: <Chiefs />,
+        },
+        {
+          path: "/roleGroup/:id",
+          element: <RoleGroup />,
+        },
+        {
+          path: "/employees",
+          element: <Employees />,
+        },
+        {
+          path: "/employees/:id",
+          element: <Employee />,
+        },
+        {
+          path: "/createRole",
+          element: <CreateRole />,
+        },
+        {
+          path: "/editRole/:id",
+          element: <EditRole />,
+        },
+        {
+          path: "/createEmployee",
+          element: <CreateEmployee />,
+        },
+        {
+          path: "/editEmployee/:id",
+          element: <EditEmployee />,
+        },
+        {
+          path: "/roleDescendants/:id",
+          element: <RoleDescendants />,
+        },
+      ],
+    },
+  ]);
+
+  const [colorScheme, setColorScheme] = useLocalStorage<ColorScheme>({
+    key: "mantine-color-scheme",
+    defaultValue: "light",
+    getInitialValueInEffect: true,
+  });
+
+  const toggleColorScheme = (value?: ColorScheme) =>
+    setColorScheme(value || (colorScheme === "dark" ? "light" : "dark"));
+
+  useHotkeys([["mod+J", () => toggleColorScheme()]]);
 
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Hero />} />
-        <Route path="/team" element={<Team />} />
-      </Routes>
-    </Router>
-  )
+    <ColorSchemeProvider
+      colorScheme={colorScheme}
+      toggleColorScheme={toggleColorScheme}
+    >
+      <MantineProvider
+        theme={{ colorScheme }}
+        withGlobalStyles
+        withNormalizeCSS
+      >
+        <RouterProvider router={router} />
+      </MantineProvider>
+    </ColorSchemeProvider>
+  );
 }
 
-export default App
+export default App;
