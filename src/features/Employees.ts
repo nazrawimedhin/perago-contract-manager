1efd04d6c1698fe50f05004e91f1daf276fd3e3d
Nazrawi Medhin <nazrawimedhin1@gmail.com>
2023-08-06 16:18:07 +0300

    redux states

---
+++ src/features/Employees.ts
@@ -0,0 +1,36 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+const initialState = {
+  loading: false,
+  data: null,
+  error: "",
+};
+
+export const fetchEmployees = createAsyncThunk("fetchEmployees", async (page) => {
+  const response = await axios.get(`http://localhost:3000/employees?page=${page}`);
+  return response.data;
+});
+
+export const employeesSlice = createSlice({
+  name: "employees",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchEmployees.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(fetchEmployees.fulfilled, (state, action) => {
+        state.loading = false;
+        state.data = action.payload
+      })
+      .addCase(fetchEmployees.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.error.message;
+      });
+  },
+});
+
+export default employeesSlice.reducer;
---
+++ src/features/Roles.ts
@@ -0,0 +1,36 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+const initialState = {
+  loading: false,
+  roles: [],
+  error: "",
+};
+
+export const fetchRoles = createAsyncThunk("fetchRoles", async () => {
+  const response = await axios.get("http://localhost:3000/roles");
+  return response.data;
+});
+
+export const rolesSlice = createSlice({
+  name: "roles",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchRoles.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(fetchRoles.fulfilled, (state, action) => {
+        state.loading = false;
+        state.roles = action.payload;
+      })
+      .addCase(fetchRoles.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.error.message;
+      });
+  },
+});
+
+export default rolesSlice.reducer;
