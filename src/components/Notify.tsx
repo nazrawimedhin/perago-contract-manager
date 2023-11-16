be0b13a2268e9ba6ffe2bb4e8812f89698c8f5b8
Nazrawi Medhin <nazrawimedhin1@gmail.com>
2023-11-16 17:51:15 +0300

    notification component

---
+++ src/components/Notify.tsx
@@ -0,0 +1,45 @@
+import { Notification, Dialog } from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
+import { BsCheckCircleFill } from "react-icons/bs";
+import { BiSolidErrorCircle } from "react-icons/bi";
+import { useSelector } from "react-redux";
+
+function Notify() {
+
+  const status = useSelector((state) => state.status);
+  const [opened, { toggle, close }] = useDisclosure(!status);
+
+  if (!status) {
+    return null;
+  }
+
+  const data = {
+    success: { color: "green", icon: <BsCheckCircleFill size="1.2rem" /> },
+    error: { color: "red", icon: <BiSolidErrorCircle size="1.2rem" /> },
+    load: { color: "green" },
+  };
+
+  const { title, message, type } = status;
+
+  return (
+    <Dialog
+      opened={opened}
+      onClose={close}
+      size="lg"
+      radius="lg"
+    >
+      <Notification
+        my={15}
+        radius='lg'
+        onClose={close}
+        title={title}
+        loading={type === "load"}
+        {...data[type]}
+      >
+        {message}
+      </Notification>
+    </Dialog>
+  );
+}
+
+export default Notify;
